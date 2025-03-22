0b7f3f470317fc182e4bc1c191b3f775b3f74f51
Toan Duong <dktoan03@gmail.com>
2025-03-22 22:30:27 +0700

    init TCP server

--- core/main.go
+++ core/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	ln, err := net.Listen("tcp", ":8081")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("MemKV server running on port 8081...")
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	fmt.Fprintf(conn, "Welcome to MemKV!\n")
 }
